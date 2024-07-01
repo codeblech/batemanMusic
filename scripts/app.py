c51ad9f175bf0f4f0a56e6df6257e7f960240cde
codeblech <my.malikyash@gmail.com>
2024-07-02 01:17:21 +0530

    working upload+url

--- scripts/app.py
+++ scripts/app.py
@@ -3,26 +3,77 @@ import subprocess
 import os
 import tempfile
 
-st.write("I have to return some videotapes")
+from thumbnail import get_ytmusic_thumbnail
+
+
+def generate_output_video(bg_image_path: str) -> str:
+    """Generates the final video using a bash script that uses ffmpeg
+
+    Args:
+        background_temp_file_path (str): path to the background image
+
+    Returns:
+        str: path to the output video
+    """
+    result = subprocess.run(
+        ["scripts/generateVideo.sh", bg_image_path], capture_output=True
+    )
+    output_video_path = str(result.stdout).lstrip("b'").rstrip("\\n'")
+    return output_video_path
+
+
+def display_generated_video(output_video_path):
+    st.write(f"Generated video saved to {output_video_path}")
+    st.video(output_video_path, loop=False, autoplay=True, muted=False)
+    # Display a slider to adjust video size
+    # DEFAULT_WIDTH = 80
+    # width = st.sidebar.slider(
+    #     label="Width", min_value=0, max_value=100, value=DEFAULT_WIDTH, format="%d%%"
+    # )
+
+    # width = max(width, 0.01)
+    # side = max((100 - width) / 2, 0.01)
+
+    # _, container, _ = st.columns([side, width, side])
+    # container.video(output_video_path, loop=False, autoplay=True, muted=False)
+
+
+st.set_page_config(
+    page_title="Paul Allen", page_icon=":speaking_head_in_silhouette:", layout="centered"
+)
+
+st.subheader("Upload your own cover art!")
 uploaded_background = st.file_uploader(
-    "Upload your own cover art!",
+    "Upload image",
     type=["png", "jpg", "jpeg", "webp"],
     help="Upload the image that would be the backgroung of ouput video",
 )
+
+st.divider()
+st.subheader("OR")
+st.divider()
+
+st.subheader("Paste the YouTube Music link of your favourite song!")
+song_url = st.text_input(
+    ("YouTube Music URL"), value=None
+)
+if song_url is not None:
+    bg_image_path = get_ytmusic_thumbnail(song_url)
+    output_video_path = generate_output_video(bg_image_path)
+    display_generated_video(output_video_path)
+
 
 if uploaded_background is not None:
     # Create a temporary directory
     with tempfile.TemporaryDirectory() as cover_art_directory:
         # Save the uploaded file to the temporary directory
-        background_temp_file_path = os.path.join(cover_art_directory, uploaded_background.name)
+        background_temp_file_path = os.path.join(
+            cover_art_directory, uploaded_background.name
+        )
         with open(background_temp_file_path, "wb") as temp_file:
             temp_file.write(uploaded_background.read())
 
         st.write(f"File saved to {background_temp_file_path}")
-        result = subprocess.run(["scripts/generateVideo.sh", background_temp_file_path], capture_output=True)
-        output_video_path = str(result.stdout).lstrip("b'").rstrip("\\n'")
-        st.write(output_video_path)
-        st.write(f"Generated video saved to {output_video_path}")
-        st.video(output_video_path, loop=False, autoplay=True, muted=False)
 
-
+        output_video_path = generate_output_video(background_temp_file_path)
+        display_generated_video(output_video_path)
---
+++ scripts/thumbnail.py
@@ -0,0 +1,55 @@
+# thumbnail xpath: //*[@id="img"]
+# thumbnail full xpath: /html/body/ytmusic-app/ytmusic-app-layout/ytmusic-player-page/div/div[1]/ytmusic-player/div[1]/yt-img-shadow/img
+# next step: robustness. add code to handle when ytmusc doesnt return deprecation warning.
+# more meaning error handling
+# make the filename unique using other metadata
+# handle the landscape style thumbnails. maybe crop the bateman video according to the thumbnail. maybe keep a few versions of the bateman video ready
+# check if thumbnail for this song is already there (downloaded)
+# use logging module
+from bs4 import BeautifulSoup
+import requests
+import os
+from PIL import Image
+from io import BytesIO
+import regex
+
+
+def get_ytmusic_thumbnail(url: str) -> str | None:
+    """Get the thumbnail of a song using its youtube music url
+
+    Args:
+        link (str): YouTube Music url to the song
+
+    Returns:
+        str | None: path to the downloaded thumbail image file
+    """
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+
+    soup = BeautifulSoup(r.content, "lxml")
+    # print(f"soup: {soup.prettify()}")
+    title_tags = soup.find_all("title") # ytmusic returns two title tags in html
+
+    if "Your browser is deprecated" in str(title_tags[0]):
+        meta = soup.find("meta", {"property": "og:image"})
+        image_link = meta.get("content", None)
+
+    if image_link is not None:
+        rr = requests.get(image_link)
+        if rr.status_code != 200:
+            return None
+        save_name = regex.findall(".*=(.*)", url)[0]  # extract last part of url
+
+        save_path = os.path.join("./assets/thumbnails", f"{save_name}.jpg")
+        with Image.open(BytesIO(rr.content)) as im:
+            try:
+                im.save(save_path, format="JPEG")
+                return save_path
+            except:
+                return None
+
+
+get_ytmusic_thumbnail(
+    "https://music.youtube.com/watch?v=vQ0u09mFodw&list=PLe1TEKS7ZJ1n4ha8ILgKQHwSOz7TedObD"
+)
